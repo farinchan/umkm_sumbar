34c702f510273d7feebef508a00b863c6b389558
fajri <fajri@gariskode.com>
2024-07-15 02:07:30 +0700

    fix seo backside

--- resources/views/back/app.blade.php
+++ resources/views/back/app.blade.php
@@ -8,32 +8,18 @@
 
 <head>
     <base href="" />
-    <title>Metronic - The World's #1 Selling Bootstrap Admin Template by KeenThemes</title>
+    <title>{{ $website->name }}</title>
     <meta charset="utf-8" />
     <meta name="description"
-        content="
-            The most advanced Bootstrap 5 Admin Theme with 40 unique prebuilt layouts on Themeforest trusted by 100,000 beginners and professionals. Multi-demo,
-            Dark Mode, RTL support and complete React, Angular, Vue, Asp.Net Core, Rails, Spring, Blazor, Django, Express.js, Node.js, Flask, Symfony & Laravel versions.
-            Grab your copy now and get life-time updates for free.
-        " />
-    <meta name="keywords"
-        content="
-            metronic, bootstrap, bootstrap 5, angular, VueJs, React, Asp.Net Core, Rails, Spring, Blazor, Django, Express.js,
-            Node.js, Flask, Symfony & Laravel starter kits, admin themes, web design, figma, web development, free templates,
-            free admin themes, bootstrap theme, bootstrap template, bootstrap dashboard, bootstrap dak mode, bootstrap button,
-            bootstrap datepicker, bootstrap timepicker, fullcalendar, datatables, flaticon
-        " />
+        content="{{ strip_tags($website->about) }}" />
+    <meta name="keywords"content="{{ $website->name }}" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta property="og:locale" content="en_US" />
-    <meta property="og:type" content="article" />
-    <meta property="og:title" content="Metronic - The World's #1 Selling Bootstrap Admin Template by KeenThemes" />
-    <meta property="og:url" content="https://keenthemes.com/metronic" />
-    <meta property="og:site_name" content="Metronic by Keenthemes" />
-    <link rel="canonical"
-        href="<br />
-<b>Notice</b>:  Undefined index: HTTP_HOST in <b>/var/www/preview.keenthemes.com/keenthemes/metronic/preview/core/html/libs/Util.php</b> on line <b>558</b><br />
-http://?page=index" />
-    <link rel="shortcut icon" href="{{ asset('back/media/logos/favicon.ico') }}" />
+    <meta property="og:title" content="{{ $website->name }}" />
+    <meta property="og:url" content="{{ request()->fullUrl() }}" />
+    <meta property="og:site_name" content="{{ $website->name }}" />
+    
+    <link rel="shortcut icon" href="{{ Storage::url('images/setting/' . $website->favicon) }}" />
 
     <!--begin::Fonts(mandatory for all pages)-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" /> <!--end::Fonts-->
